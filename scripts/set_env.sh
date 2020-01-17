c76346d7927b521ae12732252d9199f549c1ffab
Kole Nunley <kole@cantina.co>
2020-01-17 09:40:04 -0500

    create script for env var setup

---
+++ scripts/set_env.sh
@@ -0,0 +1,14 @@
+ACCESS_KEY_ID=$1
+SECRET_ACCESS_KEY=$2
+SESSION_TOKEN=$3
+
+echo "export MARKOV_PRESET_FILE=\"deepracer.py\"" >> ~/.bashrc
+echo "export MODEL_S3_BUCKET=\"deepracer-model-bucket\"" >> ~/.bashrc
+echo "export MODEL_S3_PREFIX=\"model-store\"" >> ~/.bashrc
+echo "export WORLD_NAME=\"easy_track\"" >> ~/.bashrc
+echo "export ROS_AWS_REGION=\"us-east-1\"" >> ~/.bashrc
+echo "export AWS_ACCESS_KEY_ID=\"$ACCESS_KEY_ID\"" >> ~/.bashrc
+echo "export AWS_SECRET_ACCESS_KEY=\"$SECRET_ACCESS_KEY\"" >> ~/.bashrc
+echo "export AWS_SESSION_TOKEN=\"$SESSION_TOKEN\"" >> ~/.bashrc
+
+source ~/.bashrc
