e6b288aa436321d7aa5c45d0f53e432452114c96
Kole Nunley <kfnunley@gmail.com>
2020-01-17 12:28:57 -0500

    env conversion

--- simulation_ws/src/sagemaker_rl_agent/markov/environments/deeprotor_env.py
+++ simulation_ws/src/sagemaker_rl_agent/markov/environments/deeprotor_env.py
@@ -20,31 +20,25 @@ node_type = os.environ.get("NODE_TYPE", SIMULATION_WORKER)
 
 if node_type == SIMULATION_WORKER:
     import rospy
-    from ackermann_msgs.msg import AckermannDriveStamped
     from gazebo_msgs.msg import ModelState
-    from gazebo_msgs.srv import SetModelState
-
+    from gazebo_msgs.srv import SetModelState, GetModelState
+    from scipy.spatial.transform import Rotation
     from sensor_msgs.msg import Image as sensor_image
-    from deeprotor_msgs.msg import Progress
+    from mav_msgs.msg import Actuators
 
 TRAINING_IMAGE_SIZE = (160, 120)
-FINISH_LINE = 100
-
-# REWARD ENUM
-CRASHED = 0
-NO_PROGRESS = -1
-FINISHED = 10000000.0
-MAX_STEPS = 1000000
-
-# WORLD NAME
-EASY_TRACK_WORLD = 'easy_track'
-MEDIUM_TRACK_WORLD = 'medium_track'
-HARD_TRACK_WORLD = 'hard_track'
 
 # SLEEP INTERVALS
 SLEEP_AFTER_RESET_TIME_IN_SECOND = 0.5
 SLEEP_BETWEEN_ACTION_AND_REWARD_CALCULATION_TIME_IN_SECOND = 0.1
 SLEEP_WAITING_FOR_IMAGE_TIME_IN_SECOND = 0.01
+
+CRASHED = -1
+WORLD_BOUND_X = 15
+WORLD_BOUND_Y = 15
+WORLD_BOUND_Z = 15
+NO_LIFT = [0,0,0,0]
+
 
 ### Gym Env ###
 class DeepRotorEnv(gym.Env):
@@ -53,20 +47,19 @@ class DeepRotorEnv(gym.Env):
         screen_height = TRAINING_IMAGE_SIZE[1]
         screen_width = TRAINING_IMAGE_SIZE[0]
 
-        self.on_track = 0
-        self.progress = 0
-        self.yaw = 0
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.distance_from_center = 0
-        self.distance_from_border_1 = 0
-        self.distance_from_border_2 = 0
-        self.steps = 0
-        self.progress_at_beginning_of_race = 0
+        self.start_x = 0
+        self.start_y = 0
+        self.start_z = 0
 
-        # actions -> steering angle, throttle
-        self.action_space = spaces.Box(low=np.array([-1, 0]), high=np.array([+1, +1]), dtype=np.float32)
+        self.reward_in_episode = 0
+        self.reward = None
+        self.done = False
+        self.next_state = None
+        self.image = None
+        self.steps = 0
+
+        # actions -> [rotor_0, rotor_1, rotor_2, rotor_3] w/ values 0->1
+        self.action_space = spaces.Box(low=np.array([0, 0, 0, 0]), high=np.array([+1, +1, +1, +1]), dtype=np.float32)
 
         # given image from simulator
         self.observation_space = spaces.Box(low=0, high=255,
@@ -74,21 +67,19 @@ class DeepRotorEnv(gym.Env):
 
         if node_type == SIMULATION_WORKER:
             # ROS initialization
-            self.ack_publisher = rospy.Publisher('/vesc/low_level/ackermann_cmd_mux/output',
-                                                 AckermannDriveStamped, queue_size=100)
-            self.racecar_service = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            rospy.wait_for_service('/gazebo/get_model_state')
+            rospy.wait_for_service('/gazebo/set_model_state')
+            self.get_model_service = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            self.set_model_service = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            self.velocity_publisher = rospy.Publisher("/drone/command/motor_speed", Actuators, queue_size=1)
+
             rospy.init_node('rl_coach', anonymous=True)
 
             # Subscribe to ROS topics and register callbacks
-            rospy.Subscriber('/progress', Progress, self.callback_progress)
             rospy.Subscriber('/camera/zed/rgb/image_rect_color', sensor_image, self.callback_image)
-            self.world_name = rospy.get_param('WORLD_NAME')
-            self.set_waypoints()
-            self.aws_region = rospy.get_param('ROS_AWS_REGION')
 
-        self.reward_in_episode = 0
-        self.prev_progress = 0
-        self.steps = 0
+            self.world_name = rospy.get_param('WORLD_NAME')
+            self.aws_region = rospy.get_param('ROS_AWS_REGION')
 
     def reset(self):
         if node_type == SAGEMAKER_TRAINING_WORKER:
@@ -103,47 +94,31 @@ class DeepRotorEnv(gym.Env):
         self.next_state = None
         self.image = None
         self.steps = 0
-        self.prev_progress = 0
 
-        # Reset car in Gazebo
-        self.send_action(0, 0)  # set the throttle to 0
-        self.racecar_reset()
+        # Reset drone in Gazebo
+        self.send_action(NO_LIFT)  # set rotor velocities to zero
+        self.drone_reset()
 
-        self.infer_reward_state(0, 0)
+        self.infer_reward_state(NO_LIFT)
         return self.next_state
 
-    def racecar_reset(self):
-        rospy.wait_for_service('gazebo/set_model_state')
-
+    def drone_reset(self):
         modelState = ModelState()
         modelState.pose.position.z = 0
         modelState.pose.orientation.x = 0
         modelState.pose.orientation.y = 0
         modelState.pose.orientation.z = 0
-        modelState.pose.orientation.w = 0  # Use this to randomize the orientation of the car
+        modelState.pose.orientation.w = 0 
         modelState.twist.linear.x = 0
         modelState.twist.linear.y = 0
         modelState.twist.linear.z = 0
         modelState.twist.angular.x = 0
         modelState.twist.angular.y = 0
         modelState.twist.angular.z = 0
-        modelState.model_name = 'racecar'
+        modelState.model_name = 'drone'
 
-        if self.world_name.startswith(MEDIUM_TRACK_WORLD):
-            modelState.pose.position.x = -1.40
-            modelState.pose.position.y = 2.13
-        elif self.world_name.startswith(EASY_TRACK_WORLD):
-            modelState.pose.position.x = -1.44
-            modelState.pose.position.y = -0.06
-        elif self.world_name.startswith(HARD_TRACK_WORLD):
-            modelState.pose.position.x = 1.75
-            modelState.pose.position.y = 0.6
-        else:
-            raise ValueError("Unknown simulation world: {}".format(self.world_name))
-
-        self.racecar_service(modelState)
+        self.set_model_service(modelState)
         time.sleep(SLEEP_AFTER_RESET_TIME_IN_SECOND)
-        self.progress_at_beginning_of_race = self.progress
 
     def step(self, action):
         if node_type == SAGEMAKER_TRAINING_WORKER:
@@ -154,12 +129,10 @@ class DeepRotorEnv(gym.Env):
         self.done = False
         self.next_state = None
 
-        steering_angle = float(action[0])
-        throttle = float(action[1])
         self.steps += 1
-        self.send_action(steering_angle, throttle)
+        self.send_action(action)
         time.sleep(SLEEP_BETWEEN_ACTION_AND_REWARD_CALCULATION_TIME_IN_SECOND)
-        self.infer_reward_state(steering_angle, throttle)
+        self.infer_reward_state(action)
 
         info = {}  # additional data, not to be used for training
         return self.next_state, self.reward, self.done, info
@@ -167,40 +140,52 @@ class DeepRotorEnv(gym.Env):
     def callback_image(self, data):
         self.image = data
 
-    def callback_progress(self, data):
-        self.on_track = not (data.off_track)
-        self.progress = data.progress
-        self.yaw = data.yaw
-        self.x = data.x
-        self.y = data.y
-        self.z = data.z
-        self.distance_from_center = data.distance_from_center
-        self.distance_from_border_1 = data.distance_from_border_1
-        self.distance_from_border_2 = data.distance_from_border_2
+    def send_action(self, action):
+        msg = Actuators()
+        msg.angular_velocities = action
+        self.velocity_pubisher.publish(msg)
 
-    def send_action(self, steering_angle, throttle):
-        ack_msg = AckermannDriveStamped()
-        ack_msg.header.stamp = rospy.Time.now()
-        ack_msg.drive.steering_angle = steering_angle
-        ack_msg.drive.speed = throttle
-        self.ack_publisher.publish(ack_msg)
+    def reward_function(self, params):
+        target_x = params['target_x']
+        target_y = params['target_y']
+        target_z = params['target_z']
 
-    def reward_function(self, on_track, x, y, distance_from_center, car_orientation, progress, steps,
-                        throttle, steering, track_width, waypoints, closest_waypoints):
-        if distance_from_center >= 0.0 and distance_from_center <= 0.02:
-            return 1.0
-        elif distance_from_center >= 0.02 and distance_from_center <= 0.03:
-            return 0.3
-        elif distance_from_center >= 0.03 and distance_from_center <= 0.05:
-            return 0.1
-        return 1e-3  # like crashed
+        x = params['x']
+        y = params['y']
+        z = params['z']
 
-    def infer_reward_state(self, steering_angle, throttle):
+        ideal_x = params['start_x']
+        ideal_y = params['start_y']
+        ideal_z = target_z * 1.5
+
+        x_bound = params['x_bound']
+        y_bound = params['y_bound']
+        z_bound = params['z_bound']
+
+        max_dist_x = abs(-x_bound - abs(ideal_x))
+        max_dist_y = abs(-y_bound - abs(ideal_y))
+        # can only travel along positive z axis
+        max_dist_z = z_bound - ideal_z
+
+        norm_dist_x = (abs(ideal_x - x) / max_dist_x)
+        norm_dist_y = (abs(ideal_y - y) / max_dist_y)
+        norm_dist_z = (abs(ideal_z - z) / max_dist_z)
+
+        axis_weight = 0.333
+        factor_x = exp_decay(norm_dist_x) * axis_weight
+        factor_y = exp_decay(norm_dist_y) * axis_weight
+        factor_z = exp_decay(norm_dist_z) * axis_weight
+
+        reward = factor_x + factor_y + factor_z
+
+        return float(reward)
+
+    def infer_reward_state(self, action):
         # Wait till we have a image from the camera
         while not self.image:
             time.sleep(SLEEP_WAITING_FOR_IMAGE_TIME_IN_SECOND)
 
-        # Car environment spits out BGR images by default. Converting to the
+        # Camera spits out BGR images by default. Converting to the
         # image to RGB.
         image = Image.frombytes('RGB', (self.image.width, self.image.height),
                                 self.image.data, 'raw', 'BGR', 0, 1)
@@ -208,30 +193,51 @@ class DeepRotorEnv(gym.Env):
         image = image.resize(TRAINING_IMAGE_SIZE, resample=2).convert("RGB")
         state = np.array(image)
 
-        on_track = self.on_track
-        total_progress = self.progress - self.progress_at_beginning_of_race
         done = False
 
-        self.prev_progress = total_progress
+        model_state = self.get_model_service('drone', '')
+        model_orientation = Rotation.from_quat([
+            model_state.pose.orientation.x,
+            model_state.pose.orientation.y,
+            model_state.pose.orientation.z,
+            model_state.pose.orientation.w]).as_euler('zyx')
+        model_x = model_state.pose.position.x
+        model_y = model_state.pose.position.y
+        model_z = model_state.pose.position.z
 
-        if on_track != 1:
+        model_flipped = abs(model_orientation[1]) >= 2.0 or abs(model_orientation[2]) >= 2.0
+        model_out_of_bound = (abs(model_x) >= WORLD_BOUND_X) or (abs(model_y) >= WORLD_BOUND_Y) or (abs(model_z) >= WORLD_BOUND_Z)
+        model_crashed = model_flipped or model_out_of_bound
+
+        reward = 0
+        if model_crashed:
             reward = CRASHED
             done = True
-        elif total_progress >= FINISH_LINE:  # reached max waypoints
-            print("Congratulations! You finished the race!")
-            if self.steps == 0:
-                reward = 0.0
-                done = False
-            else:
-                reward = FINISHED / self.steps
-                done = True
         else:
-            reward = self.reward_function(on_track, self.x, self.y, self.distance_from_center, self.yaw,
-                                          total_progress, self.steps, throttle, steering_angle, self.road_width,
-                                          list(self.waypoints), self.get_closest_waypoint())
+            done = False
+            target_state = self.get_model_service('target', '')
+            params = {
+                'x': model_x,
+                'y': model_y,
+                'z': model_z,
+                'start_x': self.start_x,
+                'start_y': self.start_y,
+                'start_z': self.start_z,
+                'roll': model_orientation[2],
+                'pitch': model_orientation[1],
+                'yaw': model_orientation[0],
+                'target_x': target_state.pose.position.x,
+                'target_y': target_state.pose.position.y,
+                'target_z': target_state.pose.position.z,
+                'x_bound': WORLD_BOUND_X,
+                'y_bound': WORLD_BOUND_Y,
+                'z_bound': WORLD_BOUND_Z,
+                'steps': self.steps,
+            }
+            reward = self.reward_function(params)
 
-        print('Step No=%.2f' % self.steps,
-              'Step Reward=%.2f' % reward)
+        print('Step: %.2f' % self.steps,
+              'Reward: %.2f' % reward)
 
         self.reward_in_episode += reward
         self.reward = reward
@@ -252,142 +258,27 @@ class DeepRotorEnv(gym.Env):
             Namespace='AWSRoboMakerSimulation'
         )
 
-    def set_waypoints(self):
-        if self.world_name.startswith(MEDIUM_TRACK_WORLD):
-            self.waypoints = vertices = np.zeros((8, 2))
-            self.road_width = 0.50
-            vertices[0][0] = -0.99; vertices[0][1] = 2.25;
-            vertices[1][0] = 0.69;  vertices[1][1] = 2.26;
-            vertices[2][0] = 1.37;  vertices[2][1] = 1.67;
-            vertices[3][0] = 1.48;  vertices[3][1] = -1.54;
-            vertices[4][0] = 0.81;  vertices[4][1] = -2.44;
-            vertices[5][0] = -1.25; vertices[5][1] = -2.30;
-            vertices[6][0] = -1.67; vertices[6][1] = -1.64;
-            vertices[7][0] = -1.73; vertices[7][1] = 1.63;
-        elif self.world_name.startswith(EASY_TRACK_WORLD):
-            self.waypoints = vertices = np.zeros((2, 2))
-            self.road_width = 0.90
-            vertices[0][0] = -1.08;   vertices[0][1] = -0.05;
-            vertices[1][0] =  1.08;   vertices[1][1] = -0.05;
-        else:
-            self.waypoints = vertices = np.zeros((30, 2))
-            self.road_width = 0.44
-            vertices[0][0] = 1.5;     vertices[0][1] = 0.58;
-            vertices[1][0] = 5.5;     vertices[1][1] = 0.58;
-            vertices[2][0] = 5.6;     vertices[2][1] = 0.6;
-            vertices[3][0] = 5.7;     vertices[3][1] = 0.65;
-            vertices[4][0] = 5.8;     vertices[4][1] = 0.7;
-            vertices[5][0] = 5.9;     vertices[5][1] = 0.8;
-            vertices[6][0] = 6.0;     vertices[6][1] = 0.9;
-            vertices[7][0] = 6.08;    vertices[7][1] = 1.1;
-            vertices[8][0] = 6.1;     vertices[8][1] = 1.2;
-            vertices[9][0] = 6.1;     vertices[9][1] = 1.3;
-            vertices[10][0] = 6.1;    vertices[10][1] = 1.4;
-            vertices[11][0] = 6.07;   vertices[11][1] = 1.5;
-            vertices[12][0] = 6.05;   vertices[12][1] = 1.6;
-            vertices[13][0] = 6;      vertices[13][1] = 1.7;
-            vertices[14][0] = 5.9;    vertices[14][1] = 1.8;
-            vertices[15][0] = 5.75;   vertices[15][1] = 1.9;
-            vertices[16][0] = 5.6;    vertices[16][1] = 2.0;
-            vertices[17][0] = 4.2;    vertices[17][1] = 2.02;
-            vertices[18][0] = 4;      vertices[18][1] = 2.1;
-            vertices[19][0] = 2.6;    vertices[19][1] = 3.92;
-            vertices[20][0] = 2.4;    vertices[20][1] = 4;
-            vertices[21][0] = 1.2;    vertices[21][1] = 3.95;
-            vertices[22][0] = 1.1;    vertices[22][1] = 3.92;
-            vertices[23][0] = 1;      vertices[23][1] = 3.88;
-            vertices[24][0] = 0.8;    vertices[24][1] = 3.72;
-            vertices[25][0] = 0.6;    vertices[25][1] = 3.4;
-            vertices[26][0] = 0.58;   vertices[26][1] = 3.3;
-            vertices[27][0] = 0.57;   vertices[27][1] = 3.2;
-            vertices[28][0] = 1;      vertices[28][1] = 1;
-            vertices[29][0] = 1.25;   vertices[29][1] = 0.7;
-
-    def get_closest_waypoint(self):
-        res = 0
-        index = 0
-        x = self.x
-        y = self.y
-        minDistance = float('inf')
-        for row in self.waypoints:
-            distance = math.sqrt((row[0] - x) * (row[0] - x) + (row[1] - y) * (row[1] - y))
-            if distance < minDistance:
-                minDistance = distance
-                res = index
-            index = index + 1
-        return res
-
 class DeepRotorDiscreteEnv(DeepRotorEnv):
     def __init__(self):
         DeepRotorEnv.__init__(self)
 
-        # actions -> straight, left, right
-        self.action_space = spaces.Discrete(5)
+        velocity_range_min = 455
+        velocity_range_max = 460
+        velocity_value_increment = 5
+
+        velocity_values = []
+        for i in range(0, velocity_range_max - velocity_range_min + 1, velocity_value_increment):
+            velocity_values.append(velocity_range_min+i)
+        
+        #from itertools import product 
+        #velocity_sets = [list(item) for item in product(velocity_values, repeat=4)]
+        self.velocity_sets = [[value, value, value, value] for value in velocity_values] 
+
+        # actions -> index withiin discrete list of rotor velocity sets
+        self.action_space = spaces.Discrete(len(velociity_sets))
 
     def step(self, action):
 
-        # Convert discrete to continuous
-        if action == 0:  # move left
-            steering_angle = 0.8
-            throttle = 0.3
-        elif action == 1:  # move right
-            steering_angle = -0.8  # -1 #-0.5 #-1
-            throttle = 0.3
-        elif action == 2:  # straight
-            steering_angle = 0
-            throttle = 0.3
-        elif action == 3:  # move left
-            steering_angle = 0.4
-            throttle = 0.3
-        elif action == 4:  # move right
-            steering_angle = -0.4  # -1 #-0.5 #-1
-            throttle = 0.3
-        else:  # should not be here
-            raise ValueError("Invalid action")
-
-        continous_action = [steering_angle, throttle]
-
-        return super().step(continous_action)
-
-
-class DeepRotorMultiDiscreteEnv(DeepRotorEnv):
-    def __init__(self):
-        DeepRotorEnv.__init__(self)
-
-        # actions -> straight, left, right
-        self.action_space = spaces.Discrete(10)
-
-    def step(self, action):
-
-        # Convert discrete to continuous
-        if action == 0:  # straight
-            throttle = 0.3  # 0.5
-            steering_angle = 0
-        elif action == 1:
-            throttle = 0.7
-            steering_angle = 0
-        elif action == 2:
-            throttle = 1.0
-            steering_angle = 0
-        elif action == 3:
-            throttle = 0.1
-            steering_angle = 1
-        elif action == 4:  # move left
-            throttle = 0.1
-            steering_angle = -1
-        elif action == 5:  # move left
-            throttle = 0.3
-            steering_angle = 0.75
-        elif action == 6:  # move left
-            throttle = 0.3
-            steering_angle = -0.75
-        elif action == 7:  # move right
-            throttle = 0.5
-            steering_angle = 0.5
-        else:  # action == 4
-            throttle = 0.5
-            steering_angle = -0.5
-
-        continous_action = [steering_angle, throttle]
+        continous_action = self.velocity_sets[action]
 
         return super().step(continous_action)
